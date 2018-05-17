3ecae6bedcf75947a7c70ba9d96e485989c2caa3
Eduardo Silva Navarrete <enavarrete@beeldengeluid.nl>
2018-05-17 19:14:10 +0200

    #156 add toggle option in chart to switch between abs and rel values

--- app/components/stats/QuerySingleLineChart.jsx
+++ app/components/stats/QuerySingleLineChart.jsx
@@ -1,6 +1,9 @@
 import IDUtil from '../../util/IDUtil';
 import {LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Label, ResponsiveContainer, BarChart, Legend, Bar} from 'recharts';
 import TimeUtil from '../../util/TimeUtil';
+import QueryBuilder from '../search/QueryBuilder';
+import SearchAPI from '../../api/SearchAPI';
+
 /*
 See:
 	- http://rawgraphs.io/
@@ -17,19 +20,95 @@ See:
 class QuerySingleLineChart extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            viewMode: 'absolute', // Sets default view mode to absolute.
+            query: this.props.query,
+            data: this.props.data || null,
+            isSearching: false
+        }
+        this.originalData = null;
+    }
+    componentWillMount() {
+        this.originalData = this.props.data || null
+    }
+
+    onOutput(data) {
+        if (data && !data.error) {
+            this.setState({
+                isSearching: false,
+                data: data.aggregations[data.query.dateRange.field]
+            });
+        }
+    }
+
+    doSearch(query, updateUrl = false) {
+        this.setState(
+            {isSearching : true},
+            SearchAPI.search(
+                query,
+                this.props.collectionConfig,
+                this.onOutput.bind(this),
+                updateUrl
+            )
+        )
+    }
+
+    getRelativeValues() {
+        if (this.state.viewMode === 'relative') {
+            this.setState({
+                    viewMode: 'absolute',
+                    query: {
+                        ...this.state.query,
+                        term: ''
+                    }
+                }, () => {
+                    this.doSearch(this.state.query, false)
+                }
+            )
+        } else {
+            this.setState({
+                    viewMode: 'relative',
+                    query: {
+                        ...this.state.query,
+                        term: ''
+                    }
+                }, () => {
+                    this.doSearch(this.state.query, false)
+                }
+            )
+        }
     }
 
     //TODO better ID!! (include some unique part based on the query)
     render() {
-        const dataPrettyfied = this.props.data.map(function(dataRow) {
+        // console.log(this.originalData);
+        // if this.state.data draw data ELSE make a call using the props query and collectionConfig.
+        // If there is data already draw the line chart.
+        // if(this.state.data || this.state.viewMode === 'absolute') {
+        //
+        // } else {
+        //     console.log('no data')
+        //     // make a query with using this.state.query + this.props.collectionConfig
+        //     this.doSearch(this.state.query, false)
+        // }
+
+        const dataPrettyfied = this.state.data.map(function (dataRow, i) {
             const point = {};
-            point["date"] = TimeUtil.getYearFromDate(dataRow.date_millis);
-            point["count"] = dataRow.doc_count;
+            if (this.state.viewMode === 'relative') {
+                point["date"] = TimeUtil.getYearFromDate(dataRow.date_millis);
+                point["count"] = dataRow.doc_count ? (dataRow.doc_count / this.originalData[i].doc_count)*100 : 0; // compare this one to the total !!! with the previous query
+            } else {
+                point["date"] = TimeUtil.getYearFromDate(dataRow.date_millis);
+                point["count"] = dataRow.doc_count;
+            }
             return point;
-        });
+        }, this);
+        let viewModeLabel = this.state.viewMode;
 
         return (
             <div className={IDUtil.cssClassName('query-line-chart')}>
+                <button type="button" onClick={this.getRelativeValues.bind(this)} className="btn btn-primary btn-xs">{viewModeLabel}</button>
+
                 <ResponsiveContainer width="100%" height="40%">
                     <LineChart width={600} height={300} data={dataPrettyfied} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
                         <XAxis dataKey="date"/>
