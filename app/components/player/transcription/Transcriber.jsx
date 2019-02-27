a36be6efbcfcf10956ac7f94f4ba3096499f38a9
Jaap Blom <jblom@beeldengeluid.nl>
2019-02-27 10:41:05 +0100

    modernised & cleaned up the transcriber code (mostly ok, except for the css)

--- app/components/player/transcription/Transcriber.jsx
+++ app/components/player/transcription/Transcriber.jsx
@@ -2,213 +2,220 @@ import IDUtil from '../../../util/IDUtil';
 import TimeUtil from "../../../util/TimeUtil";
 import FlexPlayerUtil from "../../../util/FlexPlayerUtil";
 
-//TODO replace all the css javascript selection and replace with pure React
-class Transcriber extends React.PureComponent {
+export default class Transcriber extends React.PureComponent {
 
-    constructor(props) {
-        super(props);
-        //when we implement editing it's useful to have the transcript in the state
-        this.state = {
-            transcript : this.props.transcript,
-            prevSearchLength :0
-        };
-        this.userHasScrolled = false;
-        this.alertTimerId = null;
-        this.GUID = IDUtil.guid();
-    }
+	constructor(props) {
+		super(props);
+		//when we implement editing it's useful to have the transcript in the state
+		this.state = {
+			transcript : this.props.transcript,
+			prevSearchLength :0,
+			showHitCounter: false,
+			numberOfHits : 0
+		};
+		this.userHasScrolled = false;
+		this.alertTimerId = null;
+		this.searchBarRef = React.createRef();
+	}
 
-    componentDidMount() {
-        //make sure the user can still scroll through the transcript
-        const transcriptWindow = document.getElementById(this.GUID);
-        const searchHitscontainer = document.querySelector('.numberOfMatches');
+	componentDidMount = () => {
+		if(this.props.initialSearchTerm && this.props.initialSearchTerm.length > 0) {
+			this.filterList(this.props.initialSearchTerm, this.onInitialFilter.bind(this));
+		}
+	}
 
-        //TODO rewrite to proper React
-        if (transcriptWindow) {
-            transcriptWindow.onscroll = () => {
-                if (this.alertTimerId == null) {
-                    this.alertTimerId = setTimeout(() => {
-                        this.userHasScrolled = false;
-                    }, 2000);
-                } else {
-                    this.userHasScrolled = true;
-                    clearTimeout(this.alertTimerId);
-                    this.alertTimerId = setTimeout(() => {
-                        this.userHasScrolled = false;
-                    }, 2000);
-                }
-            };
-        }
-        if (searchHitscontainer) {
-            searchHitscontainer.style.display = 'none';
-        }
+	componentDidUpdate = () => {
+		const segmentId = this.findClosestSegment(Math.trunc(this.props.curPosition * 1000));
+		const line = document.getElementById(segmentId);
+		if (line && !this.userHasScrolled) {
+			line.parentNode.scrollTop = line.offsetTop - 26;
+		}
+	}
 
-        if(this.props.initialSearchTerm && this.props.initialSearchTerm.length > 0) {
-            this.filterList(this.props.initialSearchTerm, this.onInitialFilter.bind(this));
-        }
-    }
+	//FIXME get rid of the timeout
+	onInitialFilter = (transcript) => {
+		if(transcript.length > 0) {
+			setTimeout( () => {	this.gotoLine(transcript[0].sequenceNr) }, 300);
+		}
+	}
 
-    onInitialFilter() {
-        if(this.state.transcript.length > 0) {
-            this.gotoLine(this.state.transcript[0].sequenceNr);
-        }
-    }
+	//makes sure the user can still scroll through the transcript
+	onScrollInTranscript = () => {
+		if (this.alertTimerId == null) {
+			this.alertTimerId = setTimeout(() => {
+				this.userHasScrolled = false;
+			}, 2400);
+		} else {
+			this.userHasScrolled = true;
+			clearTimeout(this.alertTimerId);
+			this.alertTimerId = setTimeout(() => {
+				this.userHasScrolled = false;
+			}, 2400);
+		}
+	}
 
-    gotoLine(sequenceNr) {
-        //find object based on sequenceNr
-        this.state.transcript.find(function(element, i) {
-            if(element.sequenceNr === sequenceNr) {
-                this.userHasScrolled = false;
-                this.props.playerAPI.seek(element.start / 1000);
-            }
-        }, this);
-    }
+	//find object based on sequenceNr
+	gotoLine = sequenceNr => {
+		this.state.transcript.find((element, i) => {
+			if(element.sequenceNr === sequenceNr) {
+				this.userHasScrolled = false;
+				this.props.playerAPI.seek(element.start / 1000);
+			}
+		});
+	}
 
-    getSegmentByStartTime(time) {
-        const lines = this.state.transcript.filter(obj =>  obj.start === time);
-        if(lines.length > 0) {
-            return lines[0];
-        }
-        return null;
-    }
+	getSegmentByStartTime = (time) => {
+		const lines = this.state.transcript.filter(obj =>  obj.start === time);
+		if(lines.length > 0) {
+			return lines[0];
+		}
+		return null;
+	}
 
-    //FIXME make this one faster
-    findClosestSegment(currentTime) {
-        let index = this.state.transcript.findIndex(a => a.start >= currentTime);
+	//FIXME make this one faster
+	findClosestSegment = currentTime => {
+		let index = this.state.transcript.findIndex(a => a.start >= currentTime);
 
-        if((index === -1) && this.state.transcript.length > 0) {
-            index = 0;
-        }
-        //adjust the index to the previous item when the start time is larger than the current time
-        if(this.state.transcript[index] && this.state.transcript[index].start > currentTime) {
-            index = index <= 0 ? 0 : index -1;
-        }
-        if(this.state.transcript[index]) {
-            const segment = this.getSegmentByStartTime(this.state.transcript[index].start || 0);
-            if(segment) {
-                return segment.sequenceNr || 0;
-            }
-        }
-        return 0;
-    }
+		if((index === -1) && this.state.transcript.length > 0) {
+			index = 0;
+		}
+		//adjust the index to the previous item when the start time is larger than the current time
+		if(this.state.transcript[index] && this.state.transcript[index].start > currentTime) {
+			index = index <= 0 ? 0 : index -1;
+		}
+		if(this.state.transcript[index]) {
+			const segment = this.getSegmentByStartTime(this.state.transcript[index].start || 0);
+			if(segment) {
+				return segment.sequenceNr || 0;
+			}
+		}
+		return 0;
+	}
 
-    componentDidUpdate() {
-        const segmentId = this.findClosestSegment(Math.trunc(this.props.curPosition * 1000)),
-            line = document.getElementById(segmentId);
-        if (line && !this.userHasScrolled) {
-            line.parentNode.scrollTop = line.offsetTop - 26;
-        }
-    }
+	resetTranscriber = () => {
+		this.setState(
+			{
+				transcript: this.props.transcript,
+				prevSearchLength: 0,
+				showHitCounter : false
+			},
+			() => {
+				this.searchBarRef.current.value = '';
+			}
+		);
+	}
 
-    resetTranscriber() {
-        this.setState({
-                transcript: this.props.transcript,
-                prevSearchLength: 0
-            },
-            () => {
-                    document.querySelector('.numberOfMatches').style.display = 'none',
-                    document.querySelector('input[name="search-transcriptLine"]').value = ''
-            }
-        );
-    }
+	doFilter = event => {
+		this.filterList(event.target.value, null);
+	}
 
-    //called when typing into the search box
-    doFilter(event) {
-        this.filterList(event.target.value, null);
-    }
+	filterList = (searchTerm, callback) => {
+		if(searchTerm.length === 0) {
+			this.resetTranscriber();
+			return;
+		}
+		if (searchTerm.length > 2 || (this.state.prevSearchLength > searchTerm.length)) {
+			let regex = null;
+			let filteredTranscript = [];
+			try {
+				regex = new RegExp(searchTerm, 'gi');
+			} catch(err) {
+				//in case the user enters an illegal regular expression show 0 hits
+			}
 
-    //TODO put this in a search function, then call that function when mounted with the initial search term
-    filterList(searchTerm, callback) {
-        if(searchTerm.length === 0) {
-            this.resetTranscriber();
-            return;
-        }
-        if (searchTerm.length > 2 || (this.state.prevSearchLength > searchTerm.length)) {
-            let replacementText = '',
-                word = '',
-                copiedItem = {};
-            const regex = new RegExp(searchTerm, 'gi');
-            const updatedList = this.props.transcript.filter(function (item) {
-                return item.words.toLowerCase().search(
-                    searchTerm.toLowerCase()) !== -1;
-            }).map((item) => {
-                replacementText = "<span class='highLightText'>" + searchTerm + "</span>";
-                word = item.words.replace(regex, searchedTerm => "<span class='highLightText'>" + searchedTerm + "</span>");
-                copiedItem = Object.assign({}, item);
-                copiedItem.words = word;
-                return copiedItem;
-            });
-            if (updatedList.length !== 0) {
-                this.setState({
-                        transcript: updatedList,
-                        prevSearchLength: searchTerm.length
-                    },
-                    () => {
-                        if ((updatedList.length === 0) || (updatedList.length === this.props.transcript.length)) {
-                            document.querySelector('.numberOfMatches').style.display = 'none';
-                        } else {
-                            document.querySelector('.numberOfMatches').style.display = 'inline';
-                            document.querySelector('.numberOfHits').innerHTML = updatedList.length;
-                        }
-                        if(callback) {
-                            callback();
-                        }
-                    });
-            } else {
-                this.setState(
-                    {transcript: this.props.transcript},
-                    () => {
-                        document.querySelector('.numberOfMatches').style.display = 'none';
-                        if(callback) {
-                            callback();
-                        }
-                    }
-                );
-            }
-        }
-    }
+			if(regex) {
+				filteredTranscript = this.props.transcript.filter(item => {
+					return item.words.toLowerCase().search(searchTerm.toLowerCase()) !== -1;
+				}).map(item => {
+					const copiedItem = Object.assign({}, item);
+					copiedItem.words = item.words.replace(
+						regex, searchedTerm => "<span class='highLightText'>" + searchedTerm + "</span>"
+					);
+					return copiedItem;
+				});
+			}
 
-    /* ----------------- Rendering --------------------- */
-    render() {
-        const segmentId = this.findClosestSegment(Math.trunc(this.props.curPosition * 1000)),
-            transcriptContainer = this.state.transcript.map((obj) => {
-            const className = obj.sequenceNr === segmentId ? 'sub currentLine' : 'sub';
-            return (
-                <div
-                    id={obj.sequenceNr} className={className}
-                    onClick={this.gotoLine.bind(this, obj.sequenceNr)}>
-                    <span className="data line-start-time">
-                        {TimeUtil.formatMillisToTime(
-                            FlexPlayerUtil.timeRelativeToOnAir(obj.start / 1000, this.props.mediaObject) * 1000
-                        )}
-                    </span>
-                    <span dangerouslySetInnerHTML={{__html: obj.words}}></span>
-                </div>
-            );
-        });
-        //FIXME currently there can only be one transcriber on the screen...
-        return (
-            <div className={IDUtil.cssClassName('transcriber')}>
-                <div className="transcript_search_box">
-                    <span className="glyphicon glyphicon-search"/>
-                    <input data-transcribersearch="search-transcriptLine" type="text"
-                           onChange={this.doFilter.bind(this)} name="search-transcriptLine"
-                           placeholder="Zoek.."/>
-                    <span className="numberOfMatches">
-                        <span className="numberOfHits"/> HITS
-                        <button
-                            type="button"
-                            onClick={this.resetTranscriber.bind(this)}
-                            className="glyphicon glyphicon-remove removeTranscriptFilter"
-                            aria-label="Close"
-                        />
-                    </span>
-                </div>
-                <div id={this.GUID} className="transcriptsList">
-                    {transcriptContainer}
-                </div>
-            </div>
-        )
-    }
+			const transcript = filteredTranscript.length === 0 ? this.props.transcript : filteredTranscript;
+
+			this.setState(
+				{
+					transcript: transcript, //show the whole transcript if there are no hits
+					prevSearchLength: searchTerm.length, //not sure if this one is needed anymore
+					showHitCounter: true,
+					numberOfHits : filteredTranscript.length
+				},
+				() => {
+					if(callback) {
+						callback(transcript);
+					}
+				}
+			);
+
+		}
+	}
+
+	/* ---------------------------- RENDERING FUNCTIONS ---------------------------------- */
+
+	renderTranscriptList = (transcript, curPosition) => {
+		const segmentId = this.findClosestSegment(Math.trunc(curPosition * 1000));
+		const lines = transcript.map(line => {
+			const className = line.sequenceNr === segmentId ? 'transcript-line current-line' : 'transcript-line';
+			return (
+				<div
+					id={line.sequenceNr} className={className}
+					onClick={this.gotoLine.bind(this, line.sequenceNr)}>
+					<span className="line-start-time">
+						{TimeUtil.formatMillisToTime(
+							FlexPlayerUtil.timeRelativeToOnAir(line.start / 1000, this.props.mediaObject) * 1000
+						)}
+					</span>
+					<span dangerouslySetInnerHTML={{__html: line.words}}></span>
+				</div>
+			);
+		});
+
+		return (
+			<div className="transcript-container" onScroll={this.onScrollInTranscript}>
+				{lines}
+			</div>
+		)
+	}
+
+	renderSearchBox = (showHitCounter, numberOfHits) => {
+		const hitCounter = showHitCounter ? (
+			<span className="numberOfMatches">
+				<span>{numberOfHits} HITS</span>
+				<button
+					type="button"
+					onClick={this.resetTranscriber}
+					className="glyphicon glyphicon-remove removeTranscriptFilter"
+					aria-label="Close"
+				/>
+			</span>
+		) : null;
+		return (
+			<div className="transcriber-search-bar">
+				<span className="glyphicon glyphicon-search"/>
+				<input
+					ref={this.searchBarRef}
+					className="transcriber-search-input"
+					type="text"
+					onChange={this.doFilter}
+					placeholder="Search..."/>
+				{hitCounter}
+			</div>
+		)
+	}
+
+	/* ----------------- Rendering --------------------- */
+	render = () => {
+		const transcriptList = this.renderTranscriptList(this.state.transcript, this.props.curPosition);
+		const searchBox = this.renderSearchBox(this.state.showHitCounter, this.state.numberOfHits);
+		return (
+			<div className={IDUtil.cssClassName('transcriber')}>
+				{searchBox}
+				{transcriptList}
+			</div>
+		)
+	}
 }
-
-export default Transcriber;
