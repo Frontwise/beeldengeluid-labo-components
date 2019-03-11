fb47a613fd6068adcfaf19b208d6fee96928ac50
Eduardo Silva Navarrete <enavarrete@beeldengeluid.nl>
2019-03-11 15:01:02 +0100

    #308 Cluster fields enhancements

--- app/components/search/FieldCategoryCreator.jsx
+++ app/components/search/FieldCategoryCreator.jsx
@@ -7,6 +7,9 @@ class FieldCategoryCreator extends React.PureComponent {
 
 	constructor(props) {
 		super(props);
+        this.state = {
+            filteredCategories : this.props.collectionConfig.getCollectionStats().collection_statistics.document_types[0].fields.text
+        }
 	}
 
 	onOutput() {
@@ -28,16 +31,48 @@ class FieldCategoryCreator extends React.PureComponent {
 		}
 	}
 
+    // -1 sorts array desc, 1 sorts array asc
+    sortArray = (array, direction) => {
+        direction = direction || 1;
+        array.sort((a, b) => {
+            let comparison = 0;
+            if (a.props.children > b.props.children) {
+                comparison = 1 * direction;
+            } else if (a.props.children < b.props.children) {
+                comparison = -1 * direction;
+            }
+            return comparison;
+        })
+    };
+
+    filterFields = (arr, str) => arr.filter(item => item.includes(str));
+
+    onKeywordFilter = (e) => {
+        const stats = this.props.collectionConfig.getCollectionStats();
+        const newCategorySet = this.filterFields(stats.collection_statistics.document_types[0].fields.text, e.target.value);
+
+        this.setState({
+            filteredCategories: newCategorySet
+        })
+    };
+
 	render() {
-		const stats = this.props.collectionConfig.getCollectionStats();
-		const options = stats.collection_statistics.document_types[0].fields.text.sort().map((field, index) => {
-			return (
-				<option id={index} value={field}>{this.props.collectionConfig.toPrettyFieldName(field)}</option>
-			)
-		});
+        const options = this.state.filteredCategories.map((field, index) =>
+				<option id={index} value={field}>{this.props.collectionConfig.toPrettyFieldName(field)}</option>);
+
+        this.sortArray(options, 1);
+
 		return (
 			<div className={IDUtil.cssClassName('field-category-creator')}>
 					<label htmlFor="field_selector">Select one or more fields to include</label>
+                    <div className="input-group">
+                        <input type="text"
+                               className="form-control"
+                               placeholder="Search fields"
+                               onChange={this.onKeywordFilter}
+                        />
+                        <span className="input-group-addon btn-effect"><i className="fa fa-search"/></span>
+                    </div>
 					<select className="form-control" multiple id="field_selector">
 						{options}
 					</select>
