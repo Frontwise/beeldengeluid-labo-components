Frontwise/beeldengeluid-labo-components

commit 22fbeb6046075de5395e7e4da49d9131747e57ac

the basics seem to be working: deleting bookmarks & annotations works again. Now to test more

app/components/workspace/projects/bookmark/BookmarkTable.jsx
@@ -1,8 +1,9 @@
 import ProjectAPI from '../../../../api/ProjectAPI';
+import AnnotationAPI from '../../../../api/AnnotationAPI';
 
 import IDUtil from '../../../../util/IDUtil';
 import ComponentUtil from '../../../../util/ComponentUtil';
-import BookmarkUtil from '../../../../util/BookmarkUtil';
+//import BookmarkUtil from '../../../../util/BookmarkUtil';
 
 import AnnotationStore from '../../../../flux/AnnotationStore';
 
@@ -286,13 +287,14 @@ class BookmarkTable extends React.PureComponent {
     }
 
     //delete multiple bookmarks
-    deleteBookmarks(bookmarkIds) {
-        if(bookmarkIds) {
+    deleteBookmarks(bookmarks) {
+        if(bookmarks) {
             if (!confirm('Are you sure you want to remove the selected bookmarks and all its annotations?')) {
                 return;
             }
 
             // delete each bookmark
+            /*
             BookmarkUtil.deleteBookmarks(
                 this.state.annotations,
                 this.state.bookmarks,
@@ -309,7 +311,30 @@ class BookmarkTable extends React.PureComponent {
                         }
                         , 500);
                 }
-            )
+            )*/
+
+            console.debug('DELETING BOOKMARKS', bookmarks)
+
+            //loop through the bookmarks an each of their related parent annotations to delete all of the targets
+            bookmarks.forEach(b => {
+                b.annotationIds.forEach(annotationId => {
+                    AnnotationAPI.deleteUserAnnotation(
+                        this.props.user.id,
+                        annotationId,
+                        'target', // delete a body or a target,
+                        b.resourceId, //the target source
+                        (success) => {
+                            setTimeout(()=>{
+                                // load new data
+                                this.loadBookmarks();
+
+                                // update bookmark count in project menu
+                                this.props.loadBookmarkCount();
+                            }, 500);
+                        }
+                    );
+                })
+            })
         }
     }
 
@@ -321,7 +346,7 @@ class BookmarkTable extends React.PureComponent {
     }
 
     deleteBookmark(bookmark){
-        this.deleteBookmarks([bookmark.resourceId]);
+        this.deleteBookmarks([bookmark]);
     }
 
     makeActiveProject() {
@@ -339,6 +364,19 @@ class BookmarkTable extends React.PureComponent {
     }
 
     selectAllChange(items, e) {
+        let newSelection = this.state.selection.slice(); //copy the array
+        selectedItems.forEach(item => {
+            const found = newSelection.find(selected => selected.resourceId == item.resourceId)
+            if(!found && e.target.checked) { // add it to the selection
+                newSelection.push(item);
+            } else if (e.target.checked && found) { // remove the selected item
+                newSelection.splice(found, 1);
+            }
+        })
+        this.setState({
+            selection: newSelection
+        });
+        /*
         if (e.target.checked) {
             const newSelection = this.state.selection.slice();
             items.forEach(item => {
@@ -356,10 +394,23 @@ class BookmarkTable extends React.PureComponent {
             this.setState({
                 selection: this.state.selection.filter(item => !items.includes(item))
             });
-        }
+        }*/
     }
 
     selectItem(item, select) {
+        let newSelection = this.state.selection.slice(); //copy the array
+        const index = newSelection.findIndex(selected => {
+            return selected.resourceId == item.resourceId
+        })
+        if(index == -1 && select) { // add it to the selection
+            newSelection.push(item);
+        } else if (!select && index != -1) { // remove the selected item
+            newSelection.splice(index, 1);
+        }
+        this.setState({
+            selection: newSelection
+        });
+        /*
         if (select) {
             if (!this.state.selection.includes(item.resourceId)) {
                 // add to selection
@@ -375,7 +426,7 @@ class BookmarkTable extends React.PureComponent {
             this.setState({
                 selection: this.state.selection.filter(selected => selected !== item.resourceId)
             });
-        }
+        }*/
     }
 
     //Close itemDetails view, and refresh the data (assuming changes have been made)
@@ -492,7 +543,11 @@ class BookmarkTable extends React.PureComponent {
                             onDelete={this.deleteBookmark}
                             onExport={exportDataAsJSON}
                             onView={this.viewBookmark}
-                            selected={this.state.selection.includes(bookmark.resourceId)}
+                            selected={
+                                this.state.selection.find(
+                                    item => item.resourceId == bookmark.resourceId
+                                ) != undefined
+                            }
                             onSelect={this.selectItem}
                             showSubMediaObject={bookmark.resourceId in this.state.subMediaObject}
                             showSubSegment={bookmark.resourceId in this.state.subSegment}

app/api/AnnotationAPI.js
@@ -133,13 +133,42 @@ const AnnotationAPI = {
 		xhr.onreadystatechange = function() {
 			if (xhr.readyState == XMLHttpRequest.DONE) {
 				if(xhr.status == 200) {
-					callback(JSON.parse(xhr.responseText));
+					const resp = JSON.parse(xhr.responseText)
+					//TODO the server should return the proper status code on error!!
+					if(resp.hasOwnProperty('error')) {
+						callback([]);//return an empty list by default
+					} else {
+						callback(JSON.parse(xhr.responseText));
+					}
 				} else {
 					callback([]);//return an empty list by default
 				}
 			}
 		}
 		xhr.open("GET", url);
+		xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+		xhr.send();
+	},
+
+	//TODO test this and wire this up in the wp5-mediasuite
+	deleteUserAnnotation : function(userId, annotationId, bodyOrTarget=null, partId=null, callback) {
+		let url = _config.ANNOTATION_API_BASE + '/user/'+userId;
+		url += '/annotation/'+annotationId;
+		if(bodyOrTarget && partId) {
+			url += '/'+bodyOrTarget+'/' + partId;
+		}
+		console.debug(url);
+		const xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == XMLHttpRequest.DONE) {
+				if(xhr.status == 200) {
+					callback(JSON.parse(xhr.responseText));
+				} else {
+					callback([]);//return an empty list by default
+				}
+			}
+		}
+		xhr.open("DELETE", url);
 		xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 		xhr.send();
 	}

app/components/workspace/helpers/OptionList.js
@@ -2,6 +2,7 @@ import { AnnotationTranslator } from './AnnotationTranslator';
 
 // Create a {value/name} list based on given items and key
 export const createOptionList = (items, getValue) =>{
+    if(!items) { return []; }
     const hits = {};
     const options = [];
     items.forEach(item => {
@@ -35,6 +36,7 @@ export const createClassificationOptionList = (items, key) =>{
 
 // Create a {value/name} array list based on given bookmarks
 export const createSimpleArrayOptionList = (items, getValue) => {
+    if(!items) { return []; }
     items = items.reduce((acc, i)=>(acc.concat(getValue(i))), []);
     const hits = {};
     const options = [];
@@ -54,6 +56,7 @@ const valueSort = (list) =>{
 
 // Create a {value/name} classification list based on given annotations
 export const createAnnotationClassificationOptionList = (items, key) =>{
+    if(!items) { return []; }
     const hits = {};
     return items.reduce((a,b)=>(
         a.concat(createClassificationOptionList(b.bookmarks,key))
@@ -68,6 +71,7 @@ export const createAnnotationClassificationOptionList = (items, key) =>{
 
 // Create a {value/name} list based on given annotations of given bookmarks
 export const createAnnotationOptionList = (items) =>{
+    if(!items) { return []; }
     const hits = {};
     const list = [];
     items.forEach((i)=>{

app/components/workspace/projects/annotation/AnnotationTable.jsx
@@ -40,7 +40,7 @@ class AnnotationTable extends React.PureComponent {
 
         this.state = {
             parentAnnotations : null,
-            annotations: [],
+            annotations: [], //these are actually annotation bodies (so objects from annotation['body'])
             selection: [],
             loading: true,
             detailBookmark: null,
@@ -255,34 +255,38 @@ class AnnotationTable extends React.PureComponent {
     }
 
 
-    deleteAnnotations(annotationIds) {
-        if(annotationIds) {
+    deleteAnnotations(annotationBodies) {
+        if(annotationBodies) {
             // always ask before deleting
             let msg = 'Are you sure you want to remove the selected annotation';
-            msg += annotationIds.length == 1 ? '?' : 's?';
+            msg += annotationBodies.length == 1 ? '?' : 's?';
             if (!confirm(msg)) {
                 return;
             }
 
-            BookmarkUtil.deleteAnnotations(
-                this.state.parentAnnotations,
-                this.state.annotations,
-                annotationIds,
-                (success) => {
-                    setTimeout(()=>{
-                        // load new data
-                        this.loadAnnotations();
+            annotationBodies.forEach(body => {
+                AnnotationAPI.deleteUserAnnotation(
+                    this.props.user.id,
+                    body.parentAnnotationId,
+                    'body', // delete a body or a target,
+                    body.annotationId,
+                    (success) => {
+                        setTimeout(()=>{
+                            // load new data
+                            this.loadAnnotations();
 
-                        // update bookmark count in project menu
-                        this.props.loadBookmarkCount();
-                    }, 500);
-                }
-            )
+                            // update bookmark count in project menu
+                            this.props.loadBookmarkCount();
+                        }, 500);
+                    }
+                );
+            })
         }
     }
 
     deleteAnnotation(annotation){
-        this.deleteAnnotations([annotation.annotationId]);
+        //this.deleteAnnotations([annotation.annotationId]);
+        this.deleteAnnotations([annotation]);
     }
 
     exportAnnotationsByIds(annotationIds) {
@@ -335,46 +339,36 @@ class AnnotationTable extends React.PureComponent {
         this.setSort(e.target.value);
     }
 
-    selectAllChange(items, e) {
-        if (e.target.checked) {
-            const newSelection = this.state.selection.slice();
-            items.forEach(item => {
-                if (!newSelection.includes(item.annotationId)) {
-                    newSelection.push(item.annotationId);
-                }
-            });
-            // set
-            this.setState({
-                selection: newSelection
-            });
-        } else {
-            items = items.map(item => item.annotationId);
-            // unset
-            this.setState({
-                selection: this.state.selection.filter(item => !items.includes(item))
-            });
-        }
+    //TODO test this function
+    selectAllChange(selectedItems, e) {
+        let newSelection = this.state.selection.slice(); //copy the array
+        selectedItems.forEach(item => {
+            const found = newSelection.find(selected => selected.annotationId == item.annotationId)
+            if(!found && e.target.checked) { // add it to the selection
+                newSelection.push(item);
+            } else if (e.target.checked && found) { // remove the selected item
+                newSelection.splice(found, 1);
+            }
+        })
+        this.setState({
+            selection: newSelection
+        });
     }
 
+    //TODO test this function
     selectItem(item, select) {
-        if (select) {
-            if (!this.state.selection.includes(item.annotationId)) {
-                // add to selection
-                this.setState({
-                    selection: [...this.state.selection, item.annotationId]
-                });
-            }
-            return;
+        let newSelection = this.state.selection.slice(); //copy the array
+        const index = newSelection.findIndex(selected => {
+            return selected.annotationId == item.annotationId
+        })
+        if(index == -1 && select) { // add it to the selection
+            newSelection.push(item);
+        } else if (!select && index != -1) { // remove the selected item
+            newSelection.splice(index, 1);
         }
-
-        // remove from selection
-        if (!select) {
-            this.setState({
-                selection: this.state.selection.filter(
-                    selected => selected !== item.annotationId
-                )
-            });
-        }
+        this.setState({
+            selection: newSelection
+        });
     }
 
     // Toggle sublevel visibility
@@ -432,7 +426,11 @@ class AnnotationTable extends React.PureComponent {
                             onDelete={this.deleteAnnotation}
                             onExport={this.exportAnnotation}
                             onView={this.viewBookmark}
-                            selected={this.state.selection.includes(annotation.annotationId)}
+                            selected={
+                                this.state.selection.find(
+                                    item => item.annotationId == annotation.annotationId
+                                ) != undefined
+                            }
                             onSelect={this.selectItem}
                             showSub={annotation.annotationId in this.state.showSub}
                             toggleSub={this.toggleSub}

app/util/BookmarkUtil.js
@@ -97,61 +97,6 @@ const BookmarkUtil = {
 			});
 		});
 
-	},
-
-	deleteAnnotations(parentAnnotations, annotationList, annotationIds, callback) {
-		//initialize the list of annotations
-		let count = 0;
-		const annotations = annotationList.filter(
-			item => annotationIds.includes(item.annotationId)
-		)
-
-		annotations.forEach(annotation => {
-
-			const childCount = annotationList.filter(
-				a => a.parentAnnotationId == annotation.parentAnnotationId
-			).length;
-
-			let parentAnnotation = parentAnnotations.filter(
-				pa => pa.id == annotation.parentAnnotationId
-			);
-			parentAnnotation = parentAnnotation.length == 1 ? parentAnnotation[0] : null;
-
-			if(parentAnnotation) {
-				if(childCount == 1) {
-					//delete the parent annotation entirely since the annotation was the last of its body
-					AnnotationAPI.deleteAnnotation(parentAnnotation, data => {
-						if (data.error) {
-							console.debug(data.error);
-						}
-						if(++count == annotations.length) {
-							console.debug('Delete annotation ready, calling callback');
-							callback(true)
-						}
-					});
-				} else {
-					//update the parent annotation, removing the annotation from its body
-					parentAnnotation.body =  parentAnnotation.body.filter(
-						a => a.annotationIds != annotation.annotationId
-					);
-
-					AnnotationAPI.saveAnnotation(parentAnnotation, data => {
-						if (data.error) {
-							console.debug(data.error);
-						}
-						if(++count == annotations.length) {
-							console.debug('Delete annotation ready, calling callback');
-							callback(true)
-						}
-					});
-				}
-			} else {
-				if(++count == annotations.length) {
-					console.debug('Delete annotation ready, calling callback');
-					callback(false)
-				}
-			}
-		});
 	}
 
 }

commit bae5872133b3d087869f1d360f277f55a63e1a49

fix #332 and related bugs caused by 22fbeb6046075de5395e7e4da49d9131747e57ac
Additionally, fix error when trying to filter on bookmarks with missing objects

app/components/workspace/projects/bookmark/BookmarkTable.jsx
@@ -161,8 +161,8 @@ class BookmarkTable extends React.PureComponent {
     //Update Selection list, based on available items
     updateSelection(items) {
         this.setState({
-            selection: items.map(item => item.resourceId).filter(
-                itemId => this.state.selection.includes(itemId)
+            selection: items.filter(
+                item => this.state.selection.some((i)=>(i.resourceId == item.resourceId))
             )
         });
     }
@@ -171,14 +171,14 @@ class BookmarkTable extends React.PureComponent {
         // filter on type
         if (filter.mediaType) {
             bookmarks = bookmarks.filter(bookmark =>
-                bookmark.object.mediaTypes.includes(filter.mediaType)
+                bookmark.object && bookmark.object.mediaTypes && bookmark.object.mediaTypes.includes(filter.mediaType)
             );
         }
 
         // filter on group
         if (filter.group) {
             bookmarks = bookmarks.filter(bookmark =>
-                bookmark.groups.some((g) => (g.annotationId === filter.group))
+                bookmark.groups && bookmark.groups.some((g) => (g.annotationId === filter.group))
             );
         }
 
@@ -345,7 +345,7 @@ class BookmarkTable extends React.PureComponent {
 
     exportBookmarks(selection) {
         const data = this.state.bookmarks.filter(item =>
-            selection.includes(item.resourceId)
+            selection.some((i)=>(i.resourceId == item.resourceId))
             );
         exportDataAsJSON(data);
     }
@@ -379,7 +379,7 @@ class BookmarkTable extends React.PureComponent {
             const found = newSelection.find(selected => selected.resourceId === item.resourceId)
             if(!found && e.target.checked) { // add it to the selection
                 newSelection.push(item);
-            } else if (e.target.checked && found) { // remove the selected item
+            } else if (!e.target.checked && found) { // remove the selected item
                 newSelection.splice(found, 1);
             }
         });
@@ -489,7 +489,7 @@ class BookmarkTable extends React.PureComponent {
                         type="checkbox"
                         checked={
                             renderState.visibleItems.length > 0 && renderState.visibleItems.every(item =>
-                                item && this.state.selection.includes(item.resourceId)
+                                item && this.state.selection.some((i)=>(i.resourceId == item.resourceId))
                             )
                         }
                         onChange={this.selectAllChange.bind(this, renderState.visibleItems)}/>